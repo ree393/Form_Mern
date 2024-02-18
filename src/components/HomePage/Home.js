88bf6476faf89f18ffeb992e666a1cbc1664cca3
ree393 <reenisrivastava03@gmail.com>
2024-02-18 17:33:22 +0530

    initial

---
+++ src/components/HomePage/Home.js
@@ -0,0 +1,23 @@
+// import React from "react"
+// import "./home.css"
+
+// const Home = ({setLoginUser}) => {
+//     return (
+//         <div className="homepage">
+//             <h1>Hello Homepage</h1>
+//             <div className="button" onClick={() => setLoginUser({})} >Logout</div>
+//         </div>
+//     )
+// }
+
+// export default Home
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+    <h1>This is Home Page </h1>
+    </div>
+  )
+}
